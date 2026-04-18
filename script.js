4439d84dc85266043739bcdf09414f788cfe0ff8
mariam968-code <bisirikirwamariam968@gmail.com>
2026-04-18 06:00:49 +0300

    featadded local storage to save tasks

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
+// load tasks on page openoning
+window.onload = function(){
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach(task => createTask(task.text, task.done));
+};
+
 function addTask() {
   let input = document.getElementById("taskInput");
   let task = input.value;
 
   if(task === "") return;
 
+  createTask(task, false);
+  saveTasks();
+
+  input.value = "";
+}
+
+function createTask(taskText, isDone) {
   let li = document.createElement("li");
-// create checkbox
+
   let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.checked = isDone;
 
-// create task text
-let span = document.createElement("span");
-span.textContent = task;
+  let span = document.createElement("span");
+  span.textContent = taskText;
 
-// strike through when checked
-checkbox.onclick = function(){
-  span.style.textDecoration = checkbox.checked ?
-  "line-through" : "none";
-};
+  if(isDone) {
+    span.style.textDecoration = "line-through";
+  }
+
+  checkbox.onclick = function(){
+    span.style.textDecoration = checkbox.checked ? "line-through" : "none";
+    saveTasks();
+  };
+
 
 // create delete button
 let deleteBtn = document.createElement("button");
 deleteBtn.textContent = "❌";
 deleteBtn.onclick = function(){
   li.remove();
-}
+  saveTasks();
+};
 
 li.appendChild(checkbox);
 li.appendChild(span);
@@ -33,5 +51,18 @@ li.appendChild(deleteBtn);
 
   document.getElementById("taskList").appendChild(li);
 
-  input.value = "";
+}
+
+function saveTasks(){
+  let tasks = [];
+  let listItems = document.querySelectorAll("#taskList li");
+
+  listItems.forEach(li => {
+    let text = li.querySelector("span").textContent;
+    let done = li.querySelector("input").checked;
+
+    tasks.push({ text: text, done: done});
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
